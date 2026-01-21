f2c2b8aac2b59254477c9fc3201e2f8a5aa5ed96
Rosanchaudhary <chaudharyroshan2020@gmail.com>
2026-01-21 19:17:35 +0545

    feat:slither socket

--- socket/initCardGameSocket.js
+++ socket/initCardGameSocket.js
@@ -1,7 +1,60 @@
-import { initCardGameSocket } from "./cargameHandler/index.js";
+//import { initCardGameSocket } from "./cargameHandler/index.js";
+
+const rooms = {}; 
+
 
 export function initSocket(io) {
   io.on("connection", (socket) => {
-    initCardGameSocket(io,socket);
+    //initCardGameSocket(io, socket);
+
+    console.log("Connected:", socket.id);
+
+    socket.on("join-room", ({ roomId, player }) => {
+      socket.join(roomId);
+
+      if (!rooms[roomId]) {
+        rooms[roomId] = { players: {} };
+      }
+
+      rooms[roomId].players[socket.id] = player;
+
+      // send existing players to new user
+      socket.emit("room-state", rooms[roomId].players);
+
+      // notify others
+      socket.to(roomId).emit("player-joined", {
+        id: socket.id,
+        player,
+      });
+
+      console.log(`Player joined room ${roomId}`);
+    });
+
+    socket.on("player-update", ({ roomId, player }) => {
+      if (!rooms[roomId]) return;
+
+      rooms[roomId].players[socket.id] = player;
+
+      socket.to(roomId).emit("player-update", {
+        id: socket.id,
+        player,
+      });
+    });
+
+    socket.on("disconnect", () => {
+      for (const roomId in rooms) {
+        if (rooms[roomId].players[socket.id]) {
+          delete rooms[roomId].players[socket.id];
+
+          socket.to(roomId).emit("player-left", socket.id);
+
+          if (Object.keys(rooms[roomId].players).length === 0) {
+            delete rooms[roomId];
+          }
+        }
+      }
+
+      console.log("Disconnected:", socket.id);
+    });
   });
 }
